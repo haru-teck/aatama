305384fd58e494521192eea84c028513c0e52b86
haru-teck <ginchatoku1894@gmail.com>
2024-10-05 08:23:36 +0900

    Update 20241002123858_add_user_ref_to_patients.rb

--- db/migrate/20241002123858_add_user_ref_to_patients.rb
+++ db/migrate/20241002123858_add_user_ref_to_patients.rb
@@ -1,7 +1,20 @@
 class AddUserRefToPatients < ActiveRecord::Migration[7.0]
-  def change
-    unless column_exists?(:patients, :user_id)
-      add_reference :patients, :user, null: false, foreign_key: true
-    end
+  def up
+    add_reference :patients, :user, null: true, foreign_key: true
+
+    # 既存のレコードにデフォルトのuser_idを設定（例：最初のユーザー）
+    User.first_or_create!(email: 'default@example.com', password: 'password', password_confirmation: 'password')
+    execute <<-SQL
+      UPDATE patients
+      SET user_id = (SELECT id FROM users ORDER BY id LIMIT 1)
+      WHERE user_id IS NULL;
+    SQL
+
+    # NULLを許可しない制約を追加
+    change_column_null :patients, :user_id, false
+  end
+
+  def down
+    remove_reference :patients, :user
   end
 end
