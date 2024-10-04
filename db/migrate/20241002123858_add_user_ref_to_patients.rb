0c85e3882dc66ad8edbe8c5a80f04918eb3a2e63
haru-teck <ginchatoku1894@gmail.com>
2024-10-05 08:20:58 +0900

    Update 20241002123858_add_user_ref_to_patients.rb

--- db/migrate/20241002123858_add_user_ref_to_patients.rb
+++ db/migrate/20241002123858_add_user_ref_to_patients.rb
@@ -1,14 +1,7 @@
 class AddUserRefToPatients < ActiveRecord::Migration[7.0]
   def change
-    # user_id カラムが既に存在するため、何もしない
-    # 必要に応じて、外部キー制約のみを追加
-    unless foreign_key_exists?(:patients, :users)
-      add_foreign_key :patients, :users
-    end
-
-    # インデックスが必要で、まだ存在しない場合は追加
-    unless index_exists?(:patients, :user_id)
-      add_index :patients, :user_id
+    unless column_exists?(:patients, :user_id)
+      add_reference :patients, :user, null: false, foreign_key: true
     end
   end
 end
